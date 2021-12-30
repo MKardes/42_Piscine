a1d401aea607fbb5f8db783cb05d68d9d7e19b21
Musab <msivas5858@gmail.com>
2021-12-30 12:18:19 +0300

    Reihen

--- C_00/ex07/t_main.c
+++ C_00/ex07/t_main.c
@@ -1,6 +1,15 @@
-void	ft_putchar(char c);
+#include <stdio.h>
+
+void	ft_putnbr(int nb);
 
 int	main(void)
 {
-	ft_putnbr(423);
+	printf("\nIt should print '42' : \n");
+	ft_putnbr(42);
+	printf("\nIt should print '-2147483648' : \n");
+	ft_putnbr(-2147483648);
+	printf("\nIt should print '-4242' : \n");
+	ft_putnbr(-4242);
+	printf("\n");
+	return(0);
 }
