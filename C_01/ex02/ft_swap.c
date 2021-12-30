53a15ccc6620c5e762725a516384dec1cef9b653
Musab <msivas5858@gmail.com>
2021-12-30 13:44:18 +0300

    Reihen

--- C_01/ex02/ft_swap.c
+++ C_01/ex02/ft_swap.c
@@ -1,26 +1,8 @@
-#include <stdio.h>
-
-void ft_swap(int *a, int *b)
+void	ft_swap(int *a, int *b)
 {
-	int	*d;
-
-	*d = *a;
+	int	d;
+	
+	d = *a;
 	*a = *b;
-	*b = *d;
-}
-
-int	main(void)
-{
-	int nbr;
-	int *nbr_;
-	int c;
-	int	*c_;
-
-	nbr = 1;
-	c = 2;
-	nbr_ = &nbr;
-	c_ = &c;
-	printf("%d,%d\n",*nbr_,*c_);
-	ft_swap(nbr_,c_);
-	printf("%d,%d",*nbr_,*c_);
+	*b = d;
 }
--- C_01/ex02/main.c
+++ C_01/ex02/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+void	ft_swap(int *a, int *b);
+
 int	main(void)
 {
+	int nbr;
+        int *nbr_;
+	int c;
+	int     *c_;
+
+	nbr = 1;
+	c = 2;
+	nbr_ = &nbr;
+	c_ = &c;
+	printf("It should print '1, 2': %d, %d\n\n",*nbr_,*c_);
+	ft_swap(nbr_,c_);
+	printf("It should print '2, 1': %d, %d\n",*nbr_,*c_);
 	return(0);
 }
