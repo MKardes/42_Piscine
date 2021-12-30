8ded7bd93239866badd2d666826b6b69246fe409
Musab <msivas5858@gmail.com>
2021-12-30 13:50:53 +0300

    Reihen

--- C_01/ex03/main.c
+++ C_01/ex03/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+void	ft_div_mod(int a, int b, int *div, int *mod);
+
 int	main(void)
 {
+	int x;
+	int y;
+	int *a_;
+	int *b_;
+
+	a_ = &x;
+	b_ = &y;
+	ft_div_mod(25,2,a_,b_);
+	printf("It should print '12, 1': %d, %d\n",x,y);
 	return(0);
 }
