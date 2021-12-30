bdd593e26f8fd6646ddfdfcada966448ae1bcfc0
Musab <msivas5858@gmail.com>
2021-12-30 15:57:19 +0300

    Reihen

--- C_01/ex08/main.c
+++ C_01/ex08/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+void	ft_sort_int_tab(int *tab, int size);
+
 int	main(void)
 {
+	int	i;
+	int	a[5] = {5,1,4,2,3};
+	
+	ft_sort_int_tab(a,5);
+	printf("It should print '1, 2, 3, 4, 5': ");
+	i = 0;
+	while(i < 5)
+	{
+		printf("%d",a[i]);
+		if(i != 4)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
 	return(0);
 }
