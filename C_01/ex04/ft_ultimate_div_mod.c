f9318ec7f702c69c023ed5fafd4bdd80f79e9f85
Musab <msivas5858@gmail.com>
2021-12-30 14:25:47 +0300

    Reihen

--- C_01/ex04/ft_ultimate_div_mod.c
+++ C_01/ex04/ft_ultimate_div_mod.c
@@ -1,25 +1,8 @@
-#include <stdio.h>
-
 void	ft_ultimate_div_mod(int *a, int *b)
 {
-	int *d;
+	int d;
 
-	*d = *a / *b;
-	printf("%d",*d);
-	*a = *d;
-}
-
-int	main(void)
-{
-	int a;
-	int b;
-	int *x;
-	int *y;
-
-	a = 12;
-	b = 3;
-	x = &a;
-	y = &b;
-	ft_ultimate_div_mod(x,y);
-	printf("\n%d",a);
+	d = *a / *b;
+	*b = *a % *b;
+	*a = d;
 }
--- C_01/ex04/main.c
+++ C_01/ex04/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+void	ft_ultimate_div_mod(int *a, int *b);
+
 int	main(void)
 {
+	int a;
+	int b;
+	int *x;
+	int *y;
+
+	a = 12;
+	b = 3;
+	x = &a;
+	y = &b;
+	ft_ultimate_div_mod(x,y);
+	printf("It should print '4, 0': %d, %d\n",a,b);
 	return(0);
 }
