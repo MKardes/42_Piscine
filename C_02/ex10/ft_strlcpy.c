c519ad27b44faeecaf47449796d111439b9d342f
Musab <msivas5858@gmail.com>
2022-01-05 17:49:30 +0300

    Reihen

--- C_02/ex10/ft_strlcpy.c
+++ C_02/ex10/ft_strlcpy.c
@@ -1,24 +1,20 @@
-unsigned int	ft_strlen(char *str)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-#include <stdio.h>
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
+	unsigned int	a;
 
 	i = 0;
-	while (src[i] != '\0' && i < size - 1 && size != 0)
+	a = 0;
+	while (src[a] != '\0')
+		a++;
+	if (size != 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
 	}
-	dest[i] = '\0';
-	printf("\n%s\n\n",src);
-	return (ft_strlen(src));
+	return (a);
 }
--- C_02/ex10/main.c
+++ C_02/ex10/main.c
@@ -7,6 +7,6 @@ int	main(void)
 	char asd[] = "asdasdsadweq";
 	char bcd[] = "";
 
-	printf("It should print '12, as': %d, %s\n", ft_strlcpy(bcd, asd, 2), bcd);
+	printf("It should print '12, asd': %d, %s\n", ft_strlcpy(bcd, asd, 4), bcd);
 	return (0);
 }
