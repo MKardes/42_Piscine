8f342879206e87f940ea89652215120d8900575b
Musab <msivas5858@gmail.com>
2022-01-06 13:47:09 +0300

    Reihen

--- C_03/ex00/main.c
+++ C_03/ex00/main.c
@@ -4,15 +4,12 @@ int	ft_strcmp(char *s1, char *s2);
 
 int	main(void)
 {
-	char *a = "asdb";
-	char *b = "asdd";
-	int z;
+	char	*a = "asdb";
+	char	*c = "asdb";
+	char	*b = "asdz";
 
-	z = ft_strcmp(a,b);
-	if (z > 0)
-		printf("2.sayi kucuk");
-	else if (z == 0)
-		printf("iki sayi birbirine esit");
-	else
-		printf("1.sayi kucuk");
+	printf("It should be less than 0: %d\n",ft_strcmp(a,b));
+	printf("It should be more than 0: %d\n",ft_strcmp(b,a));
+	printf("It should equal 0: %d\n",ft_strcmp(a,c));
+	return (0);
 }
