751a4002c4e58cffbc1be66ed1f665a6649f491a
Musab <msivas5858@gmail.com>
2022-01-20 12:24:49 +0300

    Eerr

--- C_03/ex01/ft_strncmp.c
+++ C_03/ex01/ft_strncmp.c
@@ -1,16 +1,13 @@
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	unsigned int	j;
-	unsigned int	a;
+	unsigned int	i;
 
-	a = 0;
-	j = 0;
-	while ((*s1 != '\0' || *s2 != '\0') && a < n)
+	i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
-		a++;
+		if (s1[i] != s2[i])
+			return(s1[i] - s2[i]);
+		i++;
 	}
 	return (0);
+}
--- C_03/ex01/main.c
+++ C_03/ex01/main.c
@@ -4,8 +4,13 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n);
 
 int	main(void)
 {
-	char *asd = "sadab";
-	char *dsa = "sadbb";
+	char	a[] = "people";
+	char	b[] = "peopIe";
+	char	c[] = "peopIe";
 
-	printf("%d",ft_strncmp(asd,dsa,5));
+	printf("It should be more than 0: %d\n",ft_strncmp(a,b,6));
+	printf("It should be less than 0: %d\n",ft_strncmp(b,a,6));
+	printf("It should equal 0: %d\n",ft_strncmp(c,b,6));
+	printf("It should equal 0: %d\n",ft_strncmp(a,b,4));
+	return (0);
 }
