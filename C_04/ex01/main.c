a0b21894e88634b468f50683eb9211f16062bb20
Musab <msivas5858@gmail.com>
2022-01-20 15:04:01 +0300

    Eerr

--- C_04/ex01/main.c
+++ C_04/ex01/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <unistd.h>
+
+void	ft_putstr(char *str);
+
+void	function(void)
+{
+	int	i;
+	char	*a = "It should print 'Love is war.': ";
+	
+	i = 0;
+	while(a[i] != '\0')
+	{
+		write(1, &a[i], 1);
+		i++;
+	}
+}
 
 int	main(void)
 {
+	char	a[] = "Love is war.";
+
+	function();	
+	ft_putstr(a);
+	printf("\n");
 	return(0);
 }
