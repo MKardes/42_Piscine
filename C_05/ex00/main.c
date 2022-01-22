e7eef6b802e3690b867c86904f773347c2c8bff5
Musab <msivas5858@gmail.com>
2022-01-22 20:25:25 +0300

    Eerr

--- C_05/ex00/main.c
+++ C_05/ex00/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int	ft_iterative_factorial(int nb);
+
 int	main(void)
 {
+	printf("It should print '1 , 1'    : %d , %d\n", ft_iterative_factorial(0), ft_iterative_factorial(1));
+	printf("It should print '0'        : %d\n", ft_iterative_factorial(-2));
+	printf("It should print '120 , 720': %d , %d\n", ft_iterative_factorial(5), ft_iterative_factorial(6));
 	return(0);
 }
