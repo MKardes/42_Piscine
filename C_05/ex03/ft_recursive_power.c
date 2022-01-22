9d608e552d3e2c138faa3591ea4eba43b501c41a
Musab <msivas5858@gmail.com>
2022-01-22 21:18:17 +0300

    Eerr

--- C_05/ex03/ft_recursive_power.c
+++ C_05/ex03/ft_recursive_power.c
@@ -4,12 +4,7 @@ int	ft_recursive_power(int nb, int power)
 		return (1);
 	else if (power < 0)
 		return (0);
-	else
-	{
-		if (power >= 1)
-		{
-			return (nb * ft_recursive_power(nb, power - 1));
-		}
-	}
+	else if (power >= 1)
+		return (nb * ft_recursive_power(nb, power - 1));
 	return (1);
 }
--- C_05/ex03/main.c
+++ C_05/ex03/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int	ft_recursive_power(int nb, int power);
+
 int	main(void)
 {
+	printf("It should print '1'       : %d\n", ft_recursive_power(0, 0));
+	printf("It should print '0'       : %d\n", ft_recursive_power(12,-2));
+	printf("It should print '25 , 216': %d , %d\n", ft_recursive_power(5, 2), ft_recursive_power(6, 3));
 	return(0);
 }
