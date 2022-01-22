6b2e5d186cadfc3bc2ad0fe42b361dcc01d46d41
Musab <msivas5858@gmail.com>
2022-01-22 21:28:13 +0300

    Eerr

--- C_05/ex04/main.c
+++ C_05/ex04/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int	ft_fibonacci(int index);
+
 int	main(void)
 {
+	printf("It should print '-1': %d\n", ft_fibonacci(-12));
+	printf("It should print '0' : %d\n", ft_fibonacci(0));
+	printf("It should print '55': %d\n", ft_fibonacci(10));
 	return(0);
 }
