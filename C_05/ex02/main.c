2a6f242fc961d403d8cc4d1dcbef23d11725ae67
Musab <msivas5858@gmail.com>
2022-01-22 21:12:47 +0300

    Eerr

--- C_05/ex02/main.c
+++ C_05/ex02/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int	ft_iterative_power(int nb, int power);
+
 int	main(void)
 {
+	printf("It should print '1'       : %d\n", ft_iterative_power(0,0));
+	printf("It should print '0'       : %d\n", ft_iterative_power(1,-2));
+	printf("It should print '25 , 216': %d , %d\n", ft_iterative_power(5,2), ft_iterative_power(6,3));
 	return(0);
 }
