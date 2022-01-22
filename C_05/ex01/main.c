cf462bec807dfb745405b8544b01225691fc1a48
Musab <msivas5858@gmail.com>
2022-01-22 20:33:29 +0300

    Eerr

--- C_05/ex01/main.c
+++ C_05/ex01/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int	ft_recursive_factorial(int nb);
+
 int	main(void)
 {
+	printf("It should print '1 , 1'    : %d , %d\n", ft_recursive_factorial(0), ft_recursive_factorial(1));
+	printf("It should print '0'        : %d\n", ft_recursive_factorial(-2));
+	printf("It should print '120 , 720': %d , %d\n", ft_recursive_factorial(5), ft_recursive_factorial(6));
 	return(0);
 }
