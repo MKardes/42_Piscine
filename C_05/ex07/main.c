e1c8eb04bbf1b013985b4f935388740bb1ddead8
Musab <msivas5858@gmail.com>
2022-01-23 11:28:32 +0300

    Eerr

--- C_05/ex07/main.c
+++ C_05/ex07/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+int	ft_find_next_prime(int nb);
+
 int	main(void)
 {
+	printf("It should print '2' : %d\n", ft_find_next_prime(-12));
+	printf("It should print '2' : %d\n", ft_find_next_prime(2));
+	printf("It should print '53': %d\n", ft_find_next_prime(49));
+	printf("It should print '53': %d\n", ft_find_next_prime(53));
 	return(0);
 }
