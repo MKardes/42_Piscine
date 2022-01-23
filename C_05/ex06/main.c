fac24166278488cb754da758ffc2c58423c33e07
Musab <msivas5858@gmail.com>
2022-01-23 11:15:43 +0300

    Eerr

--- C_05/ex06/main.c
+++ C_05/ex06/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+int	ft_is_prime(int nb);
+
 int	main(void)
 {
+	printf("It should print '0': %d\n", ft_is_prime(12));
+	printf("It should print '1': %d\n", ft_is_prime(29));
 	return(0);
 }
