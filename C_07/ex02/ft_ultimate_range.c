963e60c75418bef623b24508f62aa2ecb16cd178
Musab <msivas5858@gmail.com>
2022-01-24 12:43:57 +0300

    Eerr

--- C_07/ex02/ft_ultimate_range.c
+++ C_07/ex02/ft_ultimate_range.c
@@ -12,7 +12,7 @@ int	ft_ultimate_range(int **range, int min, int max)
 	}
 	i = 0;
 	len = max - min;
-	range[0] = (int *)malloc(len * sizeof(int));
+	range[0] = (int *)malloc(len * sizeof(int) + 1);
 	if (!*range)
 		return (-1);
 	while (min < max)
@@ -22,24 +22,4 @@ int	ft_ultimate_range(int **range, int min, int max)
 		i++;
 	}
 	return (len);
-}
-#include<stdio.h>
-int	main()
-{
-	int **a;
-	int *b;
-	int c;
-	int len;
-	int i=0;
-
-	b = &c;
-	a = &b;
-	len = ft_ultimate_range (a, 4, 10);
-	printf("%d\n", len);
-	while(i<len)
-	{
-		printf("%d", a[0][i]);
-		i++;
-	}
-	printf("\n");
 }
--- C_07/ex02/main.c
+++ C_07/ex02/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-int	main(void)
+int	ft_ultimate_range(int **range, int min, int max);
+
+int     main(void)
 {
-	return(0);
+	int     **a;
+	int	**a1;
+	int     *b;
+	int     c;
+	int     i;
+
+	b = &c;
+	a = &b;
+	printf("It should print '0': %d\n", ft_ultimate_range(a, 12, 4));
+	printf("It should print '6 , 4-5-6-7-8-9': %d , ", ft_ultimate_range(a, 4, 10));
+	i = 0;
+	while (i < 6)
+	{
+		printf("%d", a[0][i]);
+		if (i != 5)
+			printf("-");
+		i++;
+	}
+	printf("\n");
+	return (0);
 }
